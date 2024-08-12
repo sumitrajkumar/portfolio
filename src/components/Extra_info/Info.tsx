5dab2b3aade5b593a434e820bb6292ca4f45e845
sumitrajkumar <sumit.kumar2022a@vitstudent.ac.in>
2024-08-13 00:12:50 +0530

    last last bug

--- src/components/Extra_info/Info.tsx
+++ src/components/Extra_info/Info.tsx
@@ -6,16 +6,16 @@ const Info: React.FC = () => {
     <footer className="footer">
       <div className="footer-content">
         <a href="https://www.instagram.com/sumitkumar.dev/" target="_blank" rel="noopener noreferrer">
-          <img src="public\instagram_logo.png" alt="Instagram" className="footer-icon" />
+          <img src="/instagram_logo.png" alt="Instagram" className="footer-icon" />
         </a>
         <a href="https://www.linkedin.com/in/sumit-kumar-351323262/" target="_blank" rel="noopener noreferrer">
-          <img src="public\linkdein_logo.png" alt="LinkedIn" className="footer-icon" />
+          <img src="/linkdein_logo.png" alt="LinkedIn" className="footer-icon" />
         </a>
         <a href="https://github.com/sumitrajkumar" target="_blank" rel="noopener noreferrer">
-          <img src="public\github_logo.png" alt="GitHub" className="footer-icon" />
+          <img src="/github_logo.png" alt="GitHub" className="footer-icon" />
         </a>
         <a href="https://leetcode.com/u/why404/" target="_blank" rel="noopener noreferrer">
-          <img src="public\LeetCode_logo.png" alt="LeetCode" className="footer-icon" />
+          <img src="/LeetCode_logo.png" alt="LeetCode" className="footer-icon" />
         </a>
       </div>
     </footer>
